dca5deab35af8055ce7b8d38155e8fd312407638
ronyahmmod <rony.jib@gmail.com>
2022-07-18 06:15:24 +0600

    Complete Homepage welcome screen

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Box } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Homepage from "./pages/Homepage";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Box>
   );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
-
+import React from "react";
+import { Box, Button, Divider, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 const Homepage = () => {
+  const navigate = useNavigate();
   return (
-    <div>Homepage</div>
-  )
-}
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Box
+        sx={{
+          minHeight: "450",
+          width: 500,
+          backgroundColor: "background.paper",
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          p: 2,
+        }}
+      >
+        <Typography
+          variant="h2"
+          sx={(theme) => ({
+            textAlign: "center",
 
-export default Homepage
+            animation:
+              "tracking-in-expand 0.7s cubic-bezier(0.215, 0.610, 0.355, 1.000) both",
+            animationDelay: ".5s",
+          })}
+        >
+          Welcome to
+        </Typography>
+        <Typography
+          variant="h5"
+          sx={(theme) => ({
+            textAlign: "center",
+            color: theme.palette.grey[400],
+          })}
+        >
+          JIBANNAGAR DEGREE COLLEGE
+        </Typography>
+        <Divider sx={{ width: "100%", my: 1 }} />
+        <Box sx={{ display: "flex", gap: 2, mt: 1 }}>
+          <Button
+            variant="standard"
+            color="primary"
+            onClick={() => navigate("/login")}
+          >
+            Login
+          </Button>
+          <Button variant="standard" onClick={() => navigate("/register")}>
+            Register
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Homepage;
